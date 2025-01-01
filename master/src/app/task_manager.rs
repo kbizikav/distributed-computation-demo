a61bd5541bb6b2595f7a7097663863e9e9bd3ce8
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-01 13:22:56 +0700

    clean up

--- master/src/app/task_manager.rs
+++ master/src/app/task_manager.rs
@@ -8,13 +8,13 @@ use super::{problem_generator::ProblemGenerator, utils::get_unix_timestamp};
 #[derive(Clone, Debug, PartialEq)]
 pub enum TaskStatus {
     Pending,
+    Assigned { last_heartbeat: u64, progress: f64 },
     Completed,
 }
 
 #[derive(Clone, Debug)]
 pub struct Task {
     pub id: String,
-    pub last_heartbeat: u64,
     pub problem: Problem,
     pub status: TaskStatus,
 }
@@ -38,9 +38,11 @@ impl TaskManager {
             let id = uuid::Uuid::new_v4().to_string();
             let task = Task {
                 id: id.clone(),
-                last_heartbeat: 0,
                 problem,
-                status: TaskStatus::Pending,
+                status: TaskStatus::Assigned {
+                    last_heartbeat: get_unix_timestamp(),
+                    progress: 0.0,
+                },
             };
             let mut tasks = self.tasks.write().await;
             tasks.insert(id, task.clone());
@@ -63,11 +65,20 @@ impl TaskManager {
         }
     }
 
-    pub async fn submit_heartbeat(&self, id: &str) -> anyhow::Result<()> {
+    pub async fn submit_heartbeat(&self, id: &str, progress_: f64) -> anyhow::Result<()> {
         let mut tasks = self.tasks.write().await;
         if let Some(task) = tasks.get_mut(id) {
-            task.last_heartbeat = get_unix_timestamp();
-            Ok(())
+            if let TaskStatus::Assigned {
+                last_heartbeat,
+                progress,
+            } = &mut task.status
+            {
+                *last_heartbeat = get_unix_timestamp();
+                *progress = progress_;
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Task is not assigned"))
+            }
         } else {
             Err(anyhow::anyhow!("Task not found"))
         }
@@ -75,17 +86,16 @@ impl TaskManager {
 
     pub async fn cleanup_tasks(&self) -> anyhow::Result<()> {
         let mut tasks = self.tasks.write().await;
-        let current_timestamp = get_unix_timestamp();
-        let mut tasks_to_remove = vec![];
+        let mut to_remove = vec![];
         for (id, task) in tasks.iter() {
-            if current_timestamp - task.last_heartbeat > HEART_BEAT_TIMEOUT {
-                tasks_to_remove.push(id.clone());
+            if let TaskStatus::Assigned { last_heartbeat, .. } = task.status {
+                if get_unix_timestamp() - last_heartbeat > HEART_BEAT_TIMEOUT {
+                    to_remove.push(id.clone());
+                }
             }
         }
-
-        // Remove the tasks
-        for id in tasks_to_remove {
-            tasks.remove(&id);
+        for id in to_remove {
+            tasks.get_mut(&id).unwrap().status = TaskStatus::Pending;
         }
         Ok(())
     }
