c40cd0895f37846c360c1817ca49311e2efd4037
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-02-25 18:06:02 +0700

    wip

--- common/src/task_manager.rs
+++ common/src/task_manager.rs
@@ -14,25 +14,51 @@ pub enum TaskManagerError {
 }
 
 pub struct TaskManager {
+    prefix: String,
+    ttl: usize,
+    heartbeat_ttl: usize,
     client: Client,
 }
 
 impl TaskManager {
-    pub fn new(redis_url: &str) -> Result<TaskManager> {
+    pub fn new(
+        redis_url: &str,
+        prefix: &str,
+        ttl: usize,
+        heartbeat_ttl: usize,
+    ) -> Result<TaskManager> {
         let client = Client::open(redis_url)?;
-        Ok(TaskManager { client })
+        Ok(TaskManager {
+            prefix: prefix.to_owned(),
+            ttl,
+            heartbeat_ttl,
+            client,
+        })
     }
 
     async fn get_connection(&self) -> Result<Connection> {
         Ok(self.client.get_async_connection().await?)
     }
 
+    pub async fn clear_all(&self) -> Result<()> {
+        let mut conn = self.get_connection().await?;
+        let keys: Vec<String> = conn.keys(format!("{}:*", self.prefix)).await?;
+        for key in keys {
+            conn.del::<_, ()>(key).await?;
+        }
+        Ok(())
+    }
+
     pub async fn add_task(&self, task_id: u32, task: &Task) -> Result<()> {
         let mut conn = self.get_connection().await?;
 
+        let key = format!("{}:tasks", self.prefix);
         let member = serde_json::to_string(task)?;
-        conn.zadd::<_, _, _, ()>("tasks", member, task_id as f64)
+        conn.zadd::<_, _, _, ()>(&key, member, task_id as f64)
             .await?;
+
+        // set expiration
+        conn.expire::<_, ()>(&key, self.ttl).await?;
 
         Ok(())
     }
@@ -41,9 +67,11 @@ impl TaskManager {
     pub async fn assign_task(&self, worker_id: &str) -> Result<Option<(u32, Task)>> {
         let mut conn = self.get_connection().await?;
 
+        let task_key = format!("{}:tasks", self.prefix);
+
         // get task from sorted set
         let task: Option<(String, f64)> = conn
-            .zpopmin::<&str, Vec<(String, f64)>>("tasks", 1)
+            .zpopmin::<_, Vec<(String, f64)>>(task_key, 1)
             .await?
             .into_iter()
             .next();
@@ -51,9 +79,13 @@ impl TaskManager {
         if let Some((task_json, task_id)) = task {
             // add task to worker's list
             let task: Task = serde_json::from_str(&task_json)?;
-            let key = format!("worker:{}", worker_id);
+            let key = format!("{}:worker:{}", self.prefix, worker_id);
             let member = serde_json::to_string(&task)?;
-            conn.zadd::<_, _, _, ()>(key, member, task_id).await?;
+            conn.zadd::<_, _, _, ()>(&key, member, task_id).await?;
+
+            // set expiration
+            conn.expire::<_, ()>(&key, self.ttl).await?;
+
             Ok(Some((task_id as u32, task)))
         } else {
             Ok(None)
@@ -69,14 +101,61 @@ impl TaskManager {
         let mut conn = self.get_connection().await?;
 
         // remove task from worker's list
-        let key = format!("worker:{}", worker_id);
-        conn.zrem::<_, _, ()>(key, task_id).await?;
+        let worker_key = format!("{}:worker:{}", self.prefix, worker_id);
+        conn.zrem::<_, _, ()>(worker_key, task_id).await?;
 
         // add result to sorted set
+        let results_key = format!("{}:results", self.prefix);
         let member = serde_json::to_string(result)?;
-        conn.zadd::<_, _, _, ()>("results", member, task_id as f64)
+        conn.zadd::<_, _, _, ()>(&results_key, member, task_id as f64)
             .await?;
 
+        // set expiration
+        conn.expire::<_, ()>(&results_key, self.ttl).await?;
+
+        Ok(())
+    }
+
+    pub async fn submit_heartbeat(&self, worker_id: &str) -> Result<()> {
+        let mut conn = self.get_connection().await?;
+
+        let key = format!("{}:worker:heartbeat:{}", self.prefix, worker_id);
+        conn.set::<_, _, ()>(&key, "").await?;
+
+        // set expiration
+        conn.expire::<_, ()>(&key, self.heartbeat_ttl).await?;
+
+        Ok(())
+    }
+
+    // remove inactive workers and re-queue their tasks
+    pub async fn cleanup_inactive_workers(&self) -> Result<()> {
+        let mut conn = self.get_connection().await?;
+
+        let keys: Vec<String> = conn
+            .keys(format!("{}:worker:heartbeat:*", self.prefix))
+            .await?;
+        for key in keys {
+            let ttl: i64 = conn.ttl(&key).await?;
+            if ttl < 0 {
+                let worker_id = key.split(':').last().unwrap();
+                let worker_key = format!("{}:worker:{}", self.prefix, worker_id);
+
+                // re-queue tasks
+                let tasks: Vec<(String, f64)> =
+                    conn.zrangebyscore(&worker_key, 0.0, "+inf").await?;
+                for (task_json, task_id) in tasks {
+                    let key = format!("{}:tasks", self.prefix);
+                    conn.zadd::<_, _, _, ()>(&key, task_json, task_id).await?;
+
+                    // set expiration
+                    conn.expire::<_, ()>(&key, self.ttl).await?;
+                }
+
+                // remove worker
+                conn.del::<_, ()>(worker_key).await?;
+            }
+        }
         Ok(())
     }
 }
@@ -84,13 +163,23 @@ impl TaskManager {
 #[cfg(test)]
 mod tests {
 
+    use uuid::Uuid;
+
     use super::*;
     use crate::models::Task;
 
     #[tokio::test]
     async fn test_add_task() {
-        let task_manager = TaskManager::new("redis://localhost:6379").unwrap();
+        let task_manager = TaskManager::new("redis://localhost:6379", "test", 60, 3)
+            .expect("Failed to create TaskManager");
         let task = Task { x: 42 };
         task_manager.add_task(1, &task).await.unwrap();
+
+        task_manager.clear_all().await.unwrap();
+
+        let worker_id = Uuid::new_v4().to_string();
+
+        let task = task_manager.assign_task(&worker_id).await.unwrap();
+        dbg!(&task);
     }
 }
